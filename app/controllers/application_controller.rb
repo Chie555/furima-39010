767abcd91b80c737411062b0454c1dae02b17bcc
Chie555 <adgjmp159nat@gmail.com>
2023-03-02 22:42:39 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'ea11a0f2609e4cfa3e05e2071187d60b'
+    end
+  end
+
 end
