8eb524d9399bbd7bbda53c3685060ca9c30a0ce4
Chie555 <adgjmp159nat@gmail.com>
2023-03-07 20:14:13 +0900

    カテゴリ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,20 @@ class Item < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :ship_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :ship_date_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_date_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { in: 300..9999999 }
+
 
   # アソシエーション
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :prefecture, :ship_fee, :ship_date
   # has_one :order
   # has_many :comments
 
