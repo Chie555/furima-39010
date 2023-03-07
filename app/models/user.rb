ad9b09b758ac995484331d0cb74df7ebe283f764
Chie555 <adgjmp159nat@gmail.com>
2023-03-07 13:08:00 +0900

    ユーザー管理機能（view側）、パスワードバリテーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   validates :nickname, presence: true
   validates :email, uniqueness: true
+# 6半角英数字混合
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :encrypted_password,length:{ minimum: 6,
-    message: "6字以上で入力ください。" } ,format: { with: VALID_PASSWORD_REGEX}
-  validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/,
-    message: "半角のアルファベット・数字で入力ください。" }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,
-    message: "全角の漢字・ひらがな・カタカナで入力ください。"}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,
-    message: "全角の漢字・ひらがな・カタカナで入力ください。"}
-  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-    message: "全角カタカナで入力ください。"}
-  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-    message: "全角カタカナで入力ください。"}
+  validates :password,format: { with: VALID_PASSWORD_REGEX}
+# 全角（漢字・ひらがな・カタカナ）
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+# 全角（カタカナ）
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+# 生年月日
   validates :birthday, presence: true
+
 
   has_many :items
   has_many :orders
   has_many :comments
+
 end
